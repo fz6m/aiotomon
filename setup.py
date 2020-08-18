fff8ac15c9a1531edafe7d4d6d678206abaa2ff4
fz6m <fz6meng@gmail.com>
2020-08-18 16:35:56 +0800

    docs: update readme 📚

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='aiotomon',
-    version='1.0',
+    version='1.1',
     url='https://github.com/fz6m/aiotomon',
     license='MIT License',
     author='fz6m',
